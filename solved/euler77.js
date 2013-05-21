6d787c2299deb16d298817479ea11481af0496ec
Tomas Vana <tomas@Tomas-Vanas-iMac.local>
2013-05-21 18:15:37 +0200

    solved 77

--- solved/euler77.js
+++ solved/euler77.js
@@ -22,21 +22,38 @@ var cache = {
 	2: { 0: 0, 1: 0, 2: 1 }
 };
 
+var primes = sieve(1000);
+
 // # of ways to express n as a sum of primes <= k
-function p(n, k) {
+function count(n, k) {
 	if (k > n || typeof k == "undefined")
-		return p(n, n);
+		return count(n, n);
 
 	if (cache[n] && typeof cache[n][k] != "undefined")
 		return cache[n][k];
 		
 	var res = 0;
 	
-	// idea: 
-	// fix is the biggest prime in the sum, e.g. "23"
-	// now there are p(n - 23, 23) ways to express the rest
+	if (k < 2)
+		res = n == 0 ? 1 : 0;
+	else if (k == 2)
+		res = n % 2 == 0 ? 1 : 0;
+	else {
+		// for all primes q <= k
+		for (var i = 0; i < primes.length; i++) {
+			var q = primes[i];
+			if (q > k)
+				break;
+			res += count(n - q, q);
+		}
+	}
 	
 	cache[n] = cache[n] || {};
 	cache[n][k] = res;
 	return res;
 }
+
+setTimeout(function () {
+	for (var n = 2; n <= 1000; n++)
+		console.log("count(" + n + ") = " + count(n));
+}, 2000);
